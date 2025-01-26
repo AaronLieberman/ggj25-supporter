bd52230dc5029e6fa808d156fe8895efecaa70f0
Victor Erickson <vericks101@gmail.com>
2025-01-26 00:29:43 -0800

    Pushing latest changes for tomorrow.

---
+++ GameJam25/Assets/OctosharkController.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctosharkController : MonoBehaviour
+{
+    [HideInInspector] public EntityResources EntityResources;
+
+    enum HeroState { Idle, Running }
+
+    // HeroState _heroState = HeroState.Idle;
+
+    [SerializeField] List<AudioClip> FootstepClips;
+    [SerializeField] float FootstepInterval = 0.15f;
+    float _footstepTimer = 0f;
+
+    Rigidbody2D _rigidBody;
+    SpriteRenderer _spriteRenderer;
+    Animator _animator;
+    BoxCollider2D _boxCollider;
+    AudioSource _audioSource;
+    DialogBubbleController _dialogBubble;
+    Shadow _shadow;
+
+    bool _isDivingGearEquipped = false;
+
+    private float lastSpeech;
+
+    private void Awake()
+    {
+        EntityResources = GetComponent<EntityResources>();
+    }
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _animator = GetComponentInChildren<Animator>();
+        _boxCollider = GetComponentInChildren<BoxCollider2D>();
+        _audioSource = GetComponent<AudioSource>();
+        _dialogBubble = GetComponentInChildren<DialogBubbleController>();
+        _shadow = GetComponentInChildren<Shadow>();
+    }
+
+    void Update()
+    {
+        var damageHandler = GetComponentInChildren<EntityDamageHandler>();
+        var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        bool isInHurtState = damageHandler != null && damageHandler.InHurtState;
+        spriteRenderer.color = isInHurtState
+            ? new Color(1, Mathf.PingPong(Time.time * 3, 1), Mathf.PingPong(Time.time * 3, 1))
+            : new Color(1, 1, 1);
+    }
+
+    void PlayFootsteps()
+    {
+        _footstepTimer += Time.deltaTime;
+        if (_footstepTimer >= FootstepInterval)
+        {
+            _footstepTimer = 0f;
+            _audioSource.PlayOneShot(FootstepClips[UnityEngine.Random.Range(0, FootstepClips.Count - 1)], 0.5f);
+        }
+    }
+}
